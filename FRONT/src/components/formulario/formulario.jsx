45656129bb58fb68956ef94792df79459ad736a2
Anuskota <anamangada.dev@gmail.com>
2024-02-21 23:46:26 +0100

    firt form

---
+++ FRONT/src/components/formulario/formulario.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.css";
+
+function Formulario() {
+    return (
+        
+        <form>
+            <h1>Contactanos</h1>
+            <div className="col">
+            <div className="col">
+                <label>
+                <b>Name</b>
+                </label>
+                <input
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+                placeholder="Nombre"
+                />
+            </div>
+            <div className="col">
+                <label>
+                <b>Email</b>
+                </label>
+                <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                placeholder="Email"
+                />
+            </div>
+            <div className="col">
+                <label>
+                <b>Mensaje</b>
+                </label>
+                <textarea
+                type="text"
+                className="form-control"
+                id="mensaje"
+                name="mensaje"
+                
+                ></textarea>
+            </div>
+        </div>
+        <button type="submit" className="btn btn-primary">
+            Enviar Correo
+            </button>
+        </form>
+    );
+}
+
+export default Formulario;
