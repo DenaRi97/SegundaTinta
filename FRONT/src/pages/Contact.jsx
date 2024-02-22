f6d2da815d5e11ee720772bb003b924b832ae7e4
Anuskota <anamangada.dev@gmail.com>
2024-02-22 16:58:42 +0100

    form pages

---
+++ FRONT/src/pages/Contact.jsx
@@ -0,0 +1,18 @@
+import Header from "../components/header/navdropdown";
+import AboutUs from "../components/aboutus/about";
+import Home from "../components/home/home";
+import Footer from "../components/footer/footer";
+
+const Contact = () => {
+  return (
+    <>
+      <Header />
+      <div className="contact">
+        <form />
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Contact;
