fc33db8a0448c16cfdf6419341b666237892fe25
Almudena Rivera <riveraferalmudena@gmail.com>
2024-02-22 21:10:53 +0100

    underconstruction

--- FRONT/src/components/aboutus/about.jsx
+++ FRONT/src/components/aboutus/about.jsx
@@ -8,7 +8,7 @@ const AboutUs = () => {
             <h2>El equipo de Segunda Tinta 🖋</h2>
         </div>
       
-        <div className="firstRow">
+        <div className="intro">
             <p>¡Bienvenido a nuestro marketplace de libros usados! Sumérgete en un mundo de historias por descubrir mientras conectamos a lectores apasionados como tú. <br />
             Da nuevos capítulos a tus libros favoritos. Compra y vende libros usados en nuestro marketplace dedicado a los amantes de la lectura. </p>
         </div>
---
+++ FRONT/src/components/sale/sale.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./sale.css";
+
+
+const Sale = () => {
+    return (
+      <div className="header">
+        <div className="apology">
+          <span className="construction">🚧PAGE UNDER CONSTRUCTION 🚧</span>
+          <span className="sorry"> Sorry for your troubles 🙏</span>
+        </div>
+      </div>
+    );
+  }
+  export default Sale;
